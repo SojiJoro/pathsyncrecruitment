a2cead2da2e9eb909aa7d31d8072a547cc2af33f
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-04-13 10:57:13 +0100

    Blog remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
-import '../styles/globals.css';
+import '../app/globals.css';
+import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
+import Link from 'next/link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+//             </button>
+//           </div>
+//         </div>
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
